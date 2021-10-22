16670dd370c80a7a62cba74b707a9ea21da0d574
Shaheer Khalid <skhalid8627@conestogac.on.ca>
2021-10-22 12:49:00 +0000

    First commit - Test 1_Q2

---
+++ Shaheer_Khalid_8708627_Test1_Q2.py
@@ -0,0 +1,9 @@
+#Get user input and store it in a variable
+print("PLease Enter your chosen string")
+user_input = str(input())
+
+i = 0
+for i in range (len(user_input)):  
+    i += 1
+print (i)
+
